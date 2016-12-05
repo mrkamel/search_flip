c9fe4a995e8b056f7cd07c88bbda503cd2319383
Benjamin Vetter <vetter@plainpicture.de>
2016-12-05 15:43:09 +0100

    Added test stubs for BulkTest and ResponseTest

---
+++ test/elastic_search/bulk_test.rb
@@ -0,0 +1,8 @@
+
+require File.expand_path("../../test_helper", __FILE__)
+
+class ElasticSearch::BulkTest < ElasticSearch::TestCase
+  def test_bulk
+  end
+end
+
---
+++ test/elastic_search/response_test.rb
@@ -0,0 +1,41 @@
+
+require File.expand_path("../../test_helper", __FILE__)
+
+class ElasticSearch::ResponseTest < ElasticSearch::TestCase
+  def test_total_entries
+  end
+
+  def test_curent_page
+  end
+
+  def test_total_pages
+  end
+
+  def test_previous_page
+  end
+
+  def test_next_page
+  end
+
+  def test_results
+  end
+
+  def test_hits
+  end
+
+  def test_scroll_id
+  end
+
+  def test_records
+  end
+
+  def test_ids
+  end
+
+  def test_took
+  end
+
+  def test_aggregations
+  end
+end
+
