932d14590b00300e0c848a185d55ac607c51f504
Benjamin Vetter <vetter@plainpicture.de>
2016-12-07 10:14:54 +0100

    Added tests for bulk

--- test/elastic_search/bulk_test.rb
+++ test/elastic_search/bulk_test.rb
@@ -3,6 +3,53 @@ require File.expand_path("../../test_helper", __FILE__)
 
 class ElasticSearch::BulkTest < ElasticSearch::TestCase
   def test_bulk
+    product1, product2 = create_list(:product, 2)
+
+    assert_difference "ProductIndex.total_entries", 2 do
+      ProductIndex.bulk do |bulk|
+        bulk.create product1.id, JSON.generate(ProductIndex.serialize(product1))
+        bulk.create product2.id, JSON.generate(ProductIndex.serialize(product1))
+      end
+    end
+
+    assert_difference "ProductIndex.total_entries", -2 do
+      ProductIndex.bulk do |bulk|
+        bulk.delete product1.id
+        bulk.delete product2.id
+      end
+    end
+  end
+
+  def test_bulk_with_options
+    product1, product2 = create_list(:product, 2)
+
+    ProductIndex.import [product1, product2]
+
+    assert_raises "ElasticSearch::Bulk::Error" do
+      ProductIndex.bulk do |bulk|
+        bulk.create product1.id, JSON.generate(ProductIndex.serialize(product1))
+        bulk.create product2.id, JSON.generate(ProductIndex.serialize(product1))
+      end
+    end
+
+    ProductIndex.bulk(ignore_errors: [409]) do |bulk|
+      bulk.create product1.id, JSON.generate(ProductIndex.serialize(product1))
+      bulk.create product2.id, JSON.generate(ProductIndex.serialize(product1))
+    end
+  end
+
+  def test_bulk_with_item_options
+    product = create(:product)
+
+    ProductIndex.bulk do |bulk|
+      bulk.index product.id, JSON.generate(ProductIndex.serialize(product)), version: 1, version_type: "external_gt"
+    end
+
+    assert_raises "ElasticSearch::Bulk::Error" do
+      ProductIndex.bulk do |bulk|
+        bulk.index product.id, JSON.generate(ProductIndex.serialize(product)), version: 1, version_type: "external_gt"
+      end
+    end
   end
 end
 
