507648f12de216c260a0f962f4092a208501f7c2
Benjamin Vetter <vetter@plainpicture.de>
2017-03-22 19:48:51 +0100

    Docs extended, syntax changes

--- lib/elastic_search/post_filterable_relation.rb
+++ lib/elastic_search/post_filterable_relation.rb
@@ -27,15 +27,34 @@ module ElasticSearch
       end
     end
 
+    # Adds post filters to your relation for the supplied hash composed of
+    # field-to-filter mappings which specify terms, term or range filters,
+    # depending on the type of the respective hash value, namely array, range
+    # or scalar type like Fixnum, String, etc.
+    #
+    # @example Array values
+    #   query = CommentIndex.aggregate("...")
+    #   query = query.post_where(id: [1, 2, 3], state: ["approved", "declined"])
+    #
+    # @example Range values
+    #   query = CommentIndex.aggregate("...")
+    #   query = query.post_where(created_at: Time.parse("2016-01-01") .. Time.parse("2017-01-01"))
+    #
+    # @example Scalar types
+    #   query = CommentIndex.aggregate("...")
+    #   query = query.post_where(id: 1, message: "hello world")
+    #
+    # @return [ElasticSearch::Relation] A newly created extended relation
+
     def post_where(hash)
       fresh.tap do |relation|
         relation.post_filter_values = (post_filter_values || []) + hash.collect do |key, value|
           if value.is_a?(Array)
-            { :terms => { key => value } }
+            { terms: { key => value } }
           elsif value.is_a?(Range)
-            { :range => { key => { :gte => value.min, :lte => value.max } } }
+            { range: { key => { gte: value.min, lte: value.max } } }
           else
-            { :term => { key => value } }
+            { term: { key => value } }
           end
         end
       end
@@ -45,11 +64,11 @@ module ElasticSearch
       fresh.tap do |relation|
         relation.post_filter_values = (post_filter_values || []) + hash.collect do |key, value|
           if value.is_a?(Array)
-            { :not => { :terms => { key => value } } }
+            { not: { terms: { key => value } } }
           elsif value.is_a?(Range)
-            { :not => { :range => { key => { :gte => value.min, :lte => value.max } } } }
+            { not: { range: { key => { gte: value.min, lte: value.max } } } }
           else
-            { :not => { :term => { key => value } } }
+            { not: { term: { key => value } } }
           end
         end
       end
@@ -62,15 +81,15 @@ module ElasticSearch
     end
 
     def post_range(field, options = {})
-      post_filter :range => { field => options }
+      post_filter range: { field => options }
     end
 
     def post_exists(field)
-      post_filter :exists => { :field => field }
+      post_filter exists: { field: field }
     end
 
     def post_exists_not(field)
-      post_filter :bool => { :must_not => { :exists => { :field => field }}}
+      post_filter bool: { must_not: { exists: { field: field }}}
     end
   end
 end
