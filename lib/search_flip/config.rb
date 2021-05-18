306ac83fbd2e259ca7f2ffa68037b009aed0bd1e
Benjamin Vetter <benjamin.vetter@wlw.de>
2021-05-18 21:03:34 +0200

    Allow to change json options

--- lib/search_flip/config.rb
+++ lib/search_flip/config.rb
@@ -5,6 +5,12 @@ module SearchFlip
     bulk_limit: 1_000,
     bulk_max_mb: 100,
     auto_refresh: false,
-    instrumenter: NullInstrumenter.new
+    instrumenter: NullInstrumenter.new,
+    json_options: {
+      mode: :custom,
+      use_to_json: true,
+      time_format: :xmlschema,
+      bigdecimal_as_decimal: false
+    }
   }
 end
